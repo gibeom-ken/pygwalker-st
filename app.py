31fcd2f7c935855a5ae6ab4081713ac6741e6f85
gibeom-ken <158978093+gibeom-ken@users.noreply.github.com>
2024-02-06 07:29:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,45 @@
-import pandas as pd
-import streamlit.components.v1 as components
-import streamlit as st
+import streamlit as st 
+import streamlit.components.v1 as stc 
+import pandas as pd 
+import pygwalker as pyg 
+
+# Page Configuration
+st.set_page_config(page_title="StWalker App",layout="wide")
 
 
- 
-# Adjust the width of the Streamlit page
-st.set_page_config(
-    page_title="API데이터솔루션 활용하기",
-    layout="wide"
-)
- 
-# Add Title
-st.header("API데이터솔루션 활용하기")
-st.subheader("활용하기")
+st.header("Hello ")
+
+# Load Data Fxn
+def load_data(data):
+    return pd.read_csv(data)
+
+def main():
+    st.title("Streamlit PyGWalker App")
+
+    menu = ["Home", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+        # Form
+        with st.form("upload_form"):
+            data_file = st.file_uploader("Upload a CSV File",type=["csv","txt"])
+            submitted = st.form_submit_button("Submit")
+
+        if submitted:
+            df = load_data(data_file)
+            st.dataframe(df)
+            # Visualize
+            pyg_html = pyg.walk(df,return_html=True)
+            # Render with component
+            stc.html(pyg_html,scrolling=True,height=1000)
+
+        
+        
+    
+    else:
+        st.subheader("About")
+
+    
+if __name__ == "__main__":
+    main()
